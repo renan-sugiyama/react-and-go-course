36b664e7087e927851286c61d68c6be21019cd99
Renan Sugiyama <renan.sugiyama@gmail.com>
2023-02-02 21:42:37 -0300

    section 4: movie project

--- react/src/AppClass.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-import './AppClass.css'
-
-export default class AppClass extends Component {
-  render() {
-    return <h1 className="h1-red">{this.props.msg}</h1>
-  }
-}
--- react/src/index.js
+++ react/src/index.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HelloWorld from './HelloWorld'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './components/Home'
+import Movies from './components/Movies'
+import Movie from './components/Movie'
+import ErrorPage from './components/ErrorPage'
+import Genres from './components/Genres'
+import EditMovie from './components/EditMovie'
+import ManageCatalogue from './components/ManageCatalogue'
+import GraphQL from './components/GraphQL'
+import Login from './components/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/movies', element: <Movies /> },
+      { path: '/movies/:id', element: <Movie /> },
+      { path: '/genres', element: <Genres /> },
+      { path: '/admin/movie/0', element: <EditMovie /> },
+      { path: '/manage-catalogue', element: <ManageCatalogue /> },
+      { path: '/graphql', element: <GraphQL /> },
+      { path: '/login', element: <Login /> },
+    ],
+  },
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <HelloWorld msg="Hello, from function component!" />
-        </div>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
